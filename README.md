69a7f3c3b77fc4baf5adeccfe249ffcc19530138
Theodore Hadges <theodore.hadges@columbia.edu>
2020-11-03 19:40:24 -0500

    Add index face script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Smart Door
+
+
+# CLI commands to set up Rekognition
+aws rekognition create-collection \
+    --collection-id "collectionname"
---
+++ index_faces.py
@@ -0,0 +1,48 @@
+import boto3
+
+BUCKET = "amazon-rekognition"
+KEY = "myface.jpg" # Either key in S3 or however img frame gets passed from KVS
+IMAGE_ID = KEY  
+COLLECTION = "faces"
+
+# Collection is already created, but if it wasn't we can uncomment 
+# this to create it:
+# rekognition.create_collection(CollectionId=COLLECTION)
+
+def index_faces(bucket, 
+        key, 
+        collection_id, 
+        image_id=None, attributes=(), 
+        region="us-east-1"):
+	
+    rekognition = boto3.client("rekognition", region)
+	
+        response = rekognition.index_faces(
+                Image={
+                    "S3Object": {
+                        "Bucket": bucket,
+                        "Name": key,
+                        }
+                    },
+                CollectionId=collection_id,
+                ExternalImageId=image_id,
+                DetectionAttributes=attributes,
+                )
+	
+        return response['FaceRecords']
+
+for record in index_faces(BUCKET, KEY, COLLECTION, IMAGE_ID):
+	
+        face = record['Face']
+        # details = record['FaceDetail']
+        print "Face ({}%)".format(face['Confidence'])
+	print "  FaceId: {}".format(face['FaceId'])
+	print "  ImageId: {}".format(face['ImageId'])
+
+
+"""
+	Expected output:
+	Face (99.945602417%)
+	  FaceId: dc090f86-48a4-5f09-905f-44e97fb1d455
+	  ImageId: f974c8d3-7519-5796-a08d-b96e0f2fc242
+"""
