743b3fc02e6b8952c387f2251b1a2ac226f05aee
Theodore Hadges <theodore.hadges@columbia.edu>
2020-11-07 11:16:25 -0500

    Fix stream script

---
+++ rekognition/test-kinesis-stream.sh
@@ -0,0 +1,1 @@
+aws kinesis put-record --stream-name Assignment2-KDS1 --partition-key 1 --data "Hey there"
--- video-stream/ubuntu-instructions/start-streamer.sh
+++ video-stream/ubuntu-instructions/start-streamer.sh
@@ -1,2 +1,2 @@
-gst-launch-1.0 -v v4l2src device=/dev/video2 ! videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! x264enc  bframes=0 key-int-max=45 bitrate=500 tune=zerolatency ! video/x-h264,stream-format=avc,alignment=au ! kvssink stream-name="Assignment2-KVS1" storage-size=128 access-key=$AWS_ACCESS_KEY_ID secret-key=$AWS_SECRET_ACCESS_KEY
+gst-launch-1.0 -v v4l2src device=/dev/video2 ! videoconvert ! video/x-raw,format=I420,width=640,height=480,framerate=30/1 ! x264enc bframes=0 key-int-max=45 bitrate=500 tune=zerolatency ! video/x-h264,stream-format=avc,alignment=au ! kvssink stream-name="Assignment2-KVS1" storage-size=128 access-key=$AWS_ACCESS_KEY_ID secret-key=$AWS_SECRET_ACCESS_ID aws-region=$AWS_DEFAULT_REGION
 
