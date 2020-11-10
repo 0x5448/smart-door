e030496b5e3f0ad7131f9595d227525a7e2616ae
Theodore Hadges <theodore.hadges@columbia.edu>
2020-11-09 23:25:04 -0500

    Add s3 and rekognition delete functions to LF0

--- LF0/app.py
+++ LF0/app.py
@@ -2,17 +2,21 @@
 
 import json
 import boto3
-import random
+import random 
 import datetime
 import time
 
+BUCKET = 'smart-door-image-store'
+REGION = 'us-east-1'
+COLLECTION = 'faces'
 EXPIRY_5 = 60 * 5
 
 # change to relevant s3 web url during deployment
-WP1_URL = 'https://smart-door-b1.s3.amazonaws.com/wp1.html'
+WP1_URL = 'https://smart-door-b1.s3.amazonaws.com/wp1.html' 
 
 sms_client = boto3.client('sns')
-
+s3_client = boto3.client('s3')
+rekognition = boto3.client('rekognition', REGION)
 dynamodb = boto3.resource('dynamodb')
 visitors = dynamodb.Table('visitors')
 passcodes = dynamodb.Table('passcodes')
@@ -31,13 +35,13 @@ def create_visitor(face_id:str, name:str, phone_number:str, image_name:str) -> d
         ]
     }
     return record
-
+    
 def otp():
     return str(random.randint(100001, 999999))
 
 def expiry(range=EXPIRY_5):
     return str(int(time.time()) + range)
-
+    
 def create_sms(name, otp, link=WP1_URL):
     message =  f'Hey {name}, Use OTP: {otp} at {link} to gain access. OTP will expire in 5 minutes'
     return message
@@ -47,7 +51,7 @@ def send_sms(phone_number, message):
         PhoneNumber=f'+1{phone_number}',
         Message=message
     )
-
+    
 def create_passcode(phone_number:str) -> dict:
     record = {
         'PhoneNumber': phone_number,
@@ -55,28 +59,56 @@ def create_passcode(phone_number:str) -> dict:
         'ExpTime': expiry()
     }
     return record
+    
+
+def delete_face_from_rekognition(FaceId):
+    return rekognition.delete_faces()
+        CollectionId=COLLECTION,
+        FaceIds=[FaceId]
+    )
+
+
+def delete_visitor_image_from_s3(s3_object_key):
+    return s3_client.delete_object(
+        Bucket=BUCKET,
+        Key=s3_object_key
+    )
+
+
+def visitor_is_accepted():
+    return False # TODO: write this func
 
 
 def lambda_handler(event, context):
-    name = event['name']
-    phone_number = event['phone']
-    face_id = event['faceID']
-    image_name = event['filename']
-
-    visitor = create_visitor(face_id, name, phone_number, image_name)
-    passcode = create_passcode(phone_number)
-    message = create_sms(name, passcode['OTP'])
-
-    try:
-        visitors.put_item(Item=visitor)
-        passcodes.put_item(Item=passcode)
-        send_sms(phone_number, message)
-        message = 'Approved visitor'
-    except Exception as e:
-        message = str(e)
-        print(e)
-
+    if visitor_is_accepted:
+        name = event['name']
+        phone_number = event['phone']
+        face_id = event['faceID']
+        image_name = event['filename']
+        
+        FaceId = event['FaceId'] # Rekognition FaceId (in case we need to delete it)
+        s3_object_key = event['s3_object_key'] # In case we need to delete it from s3
+    
+        visitor = create_visitor(face_id, name, phone_number, image_name)
+        passcode = create_passcode(phone_number)
+        message = create_sms(name, passcode['OTP'])
+    
+        try:
+            visitors.put_item(Item=visitor)
+            passcodes.put_item(Item=passcode)
+            send_sms(phone_number, message)
+            message = 'Approved visitor'        
+        except Exception as e:
+            message = str(e)
+            print(e)
+    
+    # Else, if user is denied, delete all images and classifier info about them
+    else:
+        delete_face_from_rekognition(FaceId)
+        delete_visitor_image_from_s3(s3_object_key)
+    
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
+
