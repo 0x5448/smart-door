3db7ad5590a74080d798b52376a44eef8e1963ed
Aravind Vaddi <aravindvaddi.github@gmail.com>
2020-11-15 20:58:57 -0500

    Add code to void used OTP and template to send name to personalize message

--- LF2/app.py
+++ LF2/app.py
@@ -12,6 +12,13 @@ from boto3.dynamodb.conditions import Key
 dynamo_resource = boto3.resource('dynamodb')
 passcodes_table = dynamo_resource.Table('passcodes')
 
+def void_otp(phone_number):
+    password = {
+        'PhoneNumber': phone_number,
+        'OTP': '-1',
+        'ExpTime': '1'
+    }
+    passcodes_table.put_item(Item=password)
 
 def otp_has_expired(timestamp):
     return int(time.time()) > int(timestamp)
@@ -38,10 +45,15 @@ def validate_otp(db_item, input_otp):
     else:
         return "GRANTED"
 
+def get_name_from_externalid(externalid):
+    pass
 
 def lambda_handler(event, context):
     input_phone = event['phone']
     input_otp = event['password']
+
+    # externalid = event['externalid']
+    # name = get_name_from_externalid(externalid)
 
     try:
         db_item = get_otp_item_by_phone(input_phone)
@@ -51,12 +63,21 @@ def lambda_handler(event, context):
     print("db_item is:", str(db_item))
 
     status = validate_otp(db_item, input_otp)
+
+    if status == 'GRANTED':
+        void_otp(input_phone)
+
     print(status)
+
+    result = {
+        'status' : status,
+        # 'name' : name
+    }
 
     return {
         'statusCode': 200,
-        #'body': json.dumps(status)
-        'body': str(status)
+        'body': json.dumps(result)
+        #'body': str(status)
     }
 
 
